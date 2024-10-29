35198d37e0bb43985b9dd395d55824953ec90c7d
pbarlar <pbarlar@g.educaand.es>
2024-10-29 10:28:21 +0100

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,6 +1,12 @@
 class Ejercicio1 {
     public static void main( String args[] ) {
-        int q=2;
-        System.out.println(q);
+        int q=223, p=17;
+        float x=1.5f, y=2.11263f;
+        String a="Hola", b="Nuevo", c="Mundo";
+        double raiz1=1.234, raiz2=6.958;
+        long contador=2147483647;
+        short indicador=12333;
+        int indice=1;
+        System.out.println(b);
     }
    }
