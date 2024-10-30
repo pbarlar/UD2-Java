6288741618b1daefc4f6db6a0b0d02431595b6c5
pbarlar <pbarlar@g.educaand.es>
2024-10-30 13:42:20 +0100

    Update Ejercicio2.java

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,7 +1,18 @@
 class Ejercicio2 {
     public static void main(String[] args) {
        byte edad;
+       int CodPostal;
        short altura;
+       char genero;
+       String nombre;
+       byte numerodeHijos;
+       byte iva;
+       byte tallacamisa;
+       short peso;
+       int precio;
+       boolean alumnoRepetidor;
+       
+       
        
     }
 }
