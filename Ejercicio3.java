6a981c8f41b153ff60caca0d5e48e8af9f3cf6d4
pbarlar <pbarlar@g.educaand.es>
2024-10-30 14:29:06 +0100

    update

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,5 +1,14 @@
 public class Ejercicio3 {
     public static void main(String[] args) {
-        
+        float a=(3/2f) + (4/3f);
+        float b=(1/(6-5f))-((3*9*1)/4f);
+        double c=(1.0/2.0)+7;
+        double d=7+(1.0/2.0);
+        double e=((3*3)/(7.2-3.9))+((6-0.9)/(5-(7*9-(3.9))));
+        double f=(7.9/9.8)+8.2;
+        double g=15+(13.82/(9.9-2));
+        double h=((4*4)/(6.2*6.2))+((5.6*5.6)/(12*12));
+        double m=(5+(7.8/21.9))/(56.9-(6.7/17.4));
+        System.out.println("El resultado es "+m);
     }
 }
