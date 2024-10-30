7ec9f6149cdca28b26a731fee8bafa7e7e42b7a7
pbarlar <pbarlar@g.educaand.es>
2024-10-30 13:18:54 +0100

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -6,7 +6,7 @@ class Ejercicio1 {
         double raiz1=1.234, raiz2=6.958;
         long contador=2147483647;
         short indicador=12333;
-        int indice=1;
+        int indice=187;
         double precio=12.359, precioFinal=34.98;
         char car1='d', car2='P';
         byte valor=127;
