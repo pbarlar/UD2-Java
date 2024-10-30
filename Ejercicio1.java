50eeeaada375cfd32d53e1b805cb4f65a7b5235f
pbarlar <pbarlar@g.educaand.es>
2024-10-30 13:16:21 +0100

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -2,11 +2,14 @@ class Ejercicio1 {
     public static void main( String args[] ) {
         int q=223, p=17;
         float x=1.5f, y=2.11263f;
-        String a="Hola", b="Nuevo", c="Mundo";
+        char a='a',b='b', c='c';
         double raiz1=1.234, raiz2=6.958;
         long contador=2147483647;
         short indicador=12333;
         int indice=1;
         double precio=12.359, precioFinal=34.98;
+        char car1='d', car2='P';
+        byte valor=127;
+        String nombre="prueba cadena de caracteres";
     }
    }
