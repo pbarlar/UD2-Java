f62b7813f33172a6f5a533eb82995a9449d846dd
pbarlar <pbarlar@g.educaand.es>
2024-10-31 13:06:28 +0100

    Create Ejercicio4.java

---
+++ Ejercicio4.java
@@ -0,0 +1,23 @@
+class Ejercicio4 {
+    public static void main(String[] args) {
+        int x=63,y=12;
+        double n=2.7,m=7.8;
+        System.out.println("A: El valor de cada valiable es x:"+x+" y:"+y+" n:"+n+" m:"+m);
+        System.out.println("B: La suma de x + y es: "+(x+y));
+        System.out.println("C: La diferencia de x - y es: "+ (x-y));
+        System.out.println("D: El producto de x * y es: "+ x*y);
+        System.out.println("E: El cociente de x / y es "+ x/y);
+        System.out.println("F: El resto de x / y es: "+ x%y);
+        System.out.println("G: La suma de n + m "+ (n+m));
+        System.out.println("H: La diferencia de n - m es: "+ (n-m));
+        System.out.println("I: El producto de n * m es: "+ n*m);
+        System.out.println("J: El cociente de n / m es "+ n/m);
+        System.out.println("K: El resto de n / m es: "+ n%m);
+        System.out.println("L: La suma de x + n es: "+ (x+n));
+        System.out.println("M: El cociente de y / m es: "+ y/m);
+        System.out.println("N: El resto de y / m es: "+ y%m);
+        System.out.println("O: El doble de x es "+x*2+ ", el doble de y es "+y*2+", el doble de n es "+n*2+" y el doble de m es "+m*2);
+        System.out.println("P: La suma de x+y+n+m es: "+(x+y+n+m));
+        System.out.println("Q: El producto de x*y*n*m "+ x*y*n*m);
+    }
+}
